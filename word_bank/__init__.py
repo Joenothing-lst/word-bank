b3ba496fcd8214a61bddf1d39e0311de653d9a65
liushuntian <912871833@qq.com>
2021-01-20 15:44:14 +0800

    :bug: 修正不能正确发送图片的bug

--- word_bank/__init__.py
+++ word_bank/__init__.py
@@ -5,6 +5,7 @@ from nonebot import on_command, on_message
 from nonebot.permission import SUPERUSER
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.event import MessageEvent, GroupMessageEvent
+from nonebot.adapters.cqhttp.message import Message
 from nonebot.adapters.cqhttp.utils import unescape
 from nonebot.adapters.cqhttp.permission import GROUP_OWNER, GROUP_ADMIN, PRIVATE_FRIEND
 
@@ -21,7 +22,7 @@ async def _(bot: Bot, event: MessageEvent):
         index = event.user_id
     msg = wb.match(index, event.raw_message, flags=1)
     if msg:
-        await bot.send(event, message=unescape(random.choice(msg)))
+        await bot.send(event, message=Message(unescape(random.choice(msg))))
 
 
 wb_set_cmd = on_command('问', permission=GROUP_ADMIN | GROUP_OWNER | PRIVATE_FRIEND | SUPERUSER, aliases={'全局问', '全局模糊问', '模糊问', })
